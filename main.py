14bb84e7654250deadc3c99207e710bfd062ffb0
SilverPung <kristofer.pawel@gmail.com>
2023-11-11 21:23:31 +0100

    tokens

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
-from __future__ import print_function
+import pandas as pd
+import requests
+from requests import get,post
+import json
 import time
-import swagger_client
-from swagger_client.rest import ApiException
-from pprint import pprint
+## Get the tokens from file to connect to Strava
+with open('strava_tokens.json') as json_file:
+    strava_tokens = json.load(json_file)
+## If access_token has expired then use the refresh_token to get the new access_token
+if strava_tokens['expires_at'] < time.time():
+#Make Strava auth API call with current refresh token
+    response = requests.post(
+                        url = 'https://www.strava.com/oauth/token',
+                        data = {
+                                'client_id': 116334,
+                                'client_secret': '73a8fb9e3ce9260295c4f312063b3a7c4fc25d5c',
+                                'grant_type': 'refresh_token',
+                                'refresh_token': strava_tokens['refresh_token']
+                                }
+                    )
+#Save response as json in new variable
+    new_strava_tokens = response.json()
+# Save new tokens to file
+    with open('strava_tokens.json', 'w') as outfile:
+        json.dump(new_strava_tokens, outfile)
+#Use new Strava tokens from now
+    strava_tokens = new_strava_tokens
+#Loop through all activities
+page = 1
+url = f"https://www.strava.com/api/v3/athlete?access_token={strava_tokens['access_token']}"
+data=get(url)
+print(data.text)
 
-# Configure OAuth2 access token for authorization: strava_oauth
-configuration = swagger_client.Configuration()
-configuration.access_token = 'YOUR_ACCESS_TOKEN'
-
-# create an instance of the API class
-api_instance = swagger_client.ActivitiesApi(swagger_client.ApiClient(configuration))
-name = 'name_example' # str | The name of the activity.
-type = 'type_example' # str | Type of activity. For example - Run, Ride etc.
-start_date_local = swagger_client.ERRORUNKNOWN() # ERRORUNKNOWN | ISO 8601 formatted date time.
-elapsed_time = 56 # int | In seconds.
-description = 'description_example' # str | Description of the activity. (optional)
-distance = 3.4 # float | In meters. (optional)
-trainer = 56 # int | Set to 1 to mark as a trainer activity. (optional)
-photo_ids = swagger_client.ERRORUNKNOWN() # ERRORUNKNOWN | List of native photo ids to attach to the activity. (optional)
-commute = 56 # int | Set to 1 to mark as commute. (optional)
-
-try:
-    # Create an Activity
-    api_response = api_instance.create_activity(name, type, start_date_local, elapsed_time, description=description, distance=distance, trainer=trainer, photo_ids=photo_ids, commute=commute)
-    pprint(api_response)
-except ApiException as e:
-    print("Exception when calling ActivitiesApi->create_activity: %s\n" % e)
+#activities.to_csv('strava_activities.csv')
